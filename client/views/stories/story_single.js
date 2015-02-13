02b210c0cdfe52c146c0c67d6347c61a8fe6749a
Ramsay Lanier <ramsay@nclud.com>
2015-02-13 14:23:43 -0500

    bug fix for author image
    Signed-off-by: Ramsay Lanier <ramsay@nclud.com>

--- client/views/stories/story_single.js
+++ client/views/stories/story_single.js
@@ -22,9 +22,6 @@ Template.storySingle.helpers({
 
 		if (voted)
 			return 'upvoted'
-	},
-	profileImage: function(){
-		return Meteor.users.findOne({username: this.authorName}).profileImage;
 	}
 })
 
