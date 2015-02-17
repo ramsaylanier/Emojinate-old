4ff58a6fdd0eecc1babdc8ae4a1c14f4d25a797c
Ramsay Lanier <ramsay@nclud.com>
2015-02-17 12:40:38 -0500

    updated sort querying
    Signed-off-by: Ramsay Lanier <ramsay@nclud.com>

--- client/views/stories/story_list.js
+++ client/views/stories/story_list.js
@@ -1,8 +1,44 @@
 Template.storyList.rendered = function(){
-	console.log('rendered');
-	Meteor.defer(function(){
-		$('.user-story').removeClass('off-page');
-	})
+	this.find('.user-stories')._uihooks = {
+		moveElement: function(node, next){
+			$('.user-story').removeClass('off-page');
+			var $node = $(node), $next = $(next);
+
+			console.log($node);
+			console.log($next);
+			var oldTop = $node.offset().top;
+			var height = $node.outerHeight(true);
+
+			// find all the elements between next and node
+			var $inBetween = $next.nextUntil(node);
+			if ($inBetween.length === 0)
+			$inBetween = $node.nextUntil(next);
+
+			// now put node in place
+			$node.insertBefore(next);
+
+			// measure new top
+			var newTop = $node.offset().top;
+
+			// move node *back* to where it was before
+			$node
+			.removeClass('animate')
+			.css('top', oldTop - newTop);
+
+			// push every other element down (or up) to put them back
+			$inBetween
+			.removeClass('animate')
+			.css('top', oldTop < newTop ? height : -1 * height)
+
+
+			// force a redraw
+			$node.offset();
+
+			// reset everything to 0, animated
+			$node.addClass('animate').css('top', 0);
+			$inBetween.addClass('animate').css('top', 0);
+		}		
+	}
 }
 
 Template.storyList.events({
@@ -29,7 +65,6 @@ Template.storyList.helpers({
 });
 
 Template.storyListExcerpt.rendered = function(){
-	console.log('rendered');
 	Meteor.defer(function(){
 		$('.user-story').removeClass('off-page');
 	})
